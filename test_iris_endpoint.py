9098e2f7df551a3aab9e9e1dd5c8df7abd2467d6
ronsperber <85753940+ronsperber@users.noreply.github.com>
2025-11-21 18:43:46 -0500

    added logging

--- test_iris_endpoint.py
+++ test_iris_endpoint.py
@@ -1,3 +1,4 @@
+import logging
 from fastapi import FastAPI, Query, Body, HTTPException
 import pandas as pd
 from config.schema import schemas
@@ -5,8 +6,27 @@ from config.train_config import TRAIN_CONFIG
 from serve.model_store import ModelStore
 model_store = ModelStore(schemas, TRAIN_CONFIG)
 model_store.load_all()
-app = FastAPI()
 
+# Just configure root logger
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+logger.handlers.clear()
+
+fh = logging.FileHandler("app.log")
+fh.setLevel(logging.INFO)
+fh.setFormatter(logging.Formatter("%(asctime)s [%(levelname)s] %(message)s"))
+
+ch = logging.StreamHandler()
+ch.setLevel(logging.INFO)
+ch.setFormatter(logging.Formatter("%(asctime)s [%(levelname)s] %(message)s"))
+
+logger.addHandler(fh)
+logger.addHandler(ch)
+app = FastAPI()
+logger.info("Starting endpoints")
+logger.info("Reading in info for models")
+model_store = ModelStore(schemas, TRAIN_CONFIG)
+model_store.load_all()
 
 
 @app.post("/predict/")
@@ -17,15 +37,19 @@ def predict(
     try :
         model, metadata, schema = model_store.get(dataset)
     except KeyError as e:
+        logger.error(str(e))
         raise HTTPException(status_code=404, detail=str(e))
     try:
         validated = schema(**features)
     except Exception as e:
+        logger.error(e)
         raise HTTPException(status_code=400, detail=str(e))
     classes = metadata["classes"]
     # Convert input to DataFrame (single row)
     X = pd.DataFrame([validated.model_dump()])
     # Run prediction
+    logger.info(f"Model for {dataset} making prediction")
     y_pred = model.predict(X)
     pred_label = classes[y_pred[0]]
+    logger.info(f"Model for {dataset} prediction returned")
     return {"predicted_label": pred_label}
