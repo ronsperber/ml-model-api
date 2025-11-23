1465c896ec407428033a9295ec0bee70b0c24aa4
ronsperber <85753940+ronsperber@users.noreply.github.com>
2025-11-23 17:29:39 -0500

    cleaned up /predict and added /predict_batch

--- app.py
+++ app.py
@@ -31,8 +31,8 @@ model_store.load_all()
 
 @app.post("/predict/")
 def predict(
-    features: dict = Body(...),
-    dataset = Query("iris", description="Dataset chosen"),
+    features:dict = Body(...),
+    dataset:str = Query("iris", description="Dataset chosen")
     ):
     try :
         model, metadata, schema = model_store.get(dataset)
@@ -60,8 +60,55 @@ def predict(
         # if no predict_proba exists, use the predict method to
         # get the prediction and use empty list for probabilities
         y_pred = model.predict(X)[0]
-        y_pred_proba_list = [0] * len(classes)
+        y_pred_proba_list = []
     pred_label = classes[y_pred]
-    probs = {k:v for (k,v) in list(zip(classes, y_pred_proba_list))}
+    probs = dict(zip(classes, y_pred_proba_list))
     logger.info(f"Model for {dataset} prediction returned")
     return {"predicted_label": pred_label, "predicted_probs": probs}
+
+@app.post("/predict_batch/")
+def predict_batch(
+    payload: dict = Body(...),
+    dataset: str = Query("iris", description="Dataset chosen")
+):
+    try :
+        model, metadata, schema = model_store.get(dataset)
+    except KeyError as e:
+        logger.error(str(e))
+        raise HTTPException(status_code=404, detail=str(e))
+    # validate payload
+    if "items" not in payload or not isinstance(payload["items"], list):
+        raise HTTPException(status_code=400, detail="Expected {'items': [...] }")
+    validated_rows = []
+    for i, row in enumerate(payload["items"]):
+        try:
+            validated = schema(**row)
+            validated_rows.append(validated.model_dump())
+        except Exception as e:
+            logger.error(f"Validation failed on row {i}: {e}")
+            raise HTTPException(status_code=400, detail=f"Invalid row {i}: {e}")
+    # convert to dataframe
+    X = pd.DataFrame(validated_rows)
+    logger.info(f"Model for {dataset} making prediction")
+    # get the classes 
+    classes = metadata["classes"]
+    # if we can predict probabilities do that
+    if hasattr(model, "predict_proba"):
+        y_proba = model.predict_proba(X)
+        # determine which is the predicted label
+        y_pred = y_proba.argmax(axis=-1)
+    else:
+        # if no predict_proba exists, use the predict method to
+        # get the prediction and use empty list for probabilities
+        y_pred = model.predict(X)
+        y_proba = None
+    response = []
+    for i, pred_idx in enumerate(y_pred):
+        probs = y_proba[i] if y_proba is not None else []
+        obj = {
+            "predicted_label": classes[pred_idx],
+            "predicted_probs": dict(zip(classes, probs))
+        }
+        response.append(obj)
+    logger.info(f"Model for {dataset} prediction returned")
+    return {"response": response}
