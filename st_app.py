cd798edbb9ec93cf5953e95bd1fe72bffb8e1395
ronsperber <85753940+ronsperber@users.noreply.github.com>
2025-11-24 17:07:48 -0500

    added basic streamlit app

---
+++ st_app.py
@@ -0,0 +1,43 @@
+import json
+import requests
+import streamlit as st
+from typing import Type
+from pydantic import BaseModel
+from pydantic_core import PydanticUndefined
+from serve.model_store import ModelStore
+from config.schema import schemas
+from config.train_config import TRAIN_CONFIG
+st.title("Model Predictions")
+model_name = st.sidebar.selectbox(label="Model name",options=list(schemas.keys()))
+schema = schemas[model_name]
+config = TRAIN_CONFIG[model_name]
+endpoint = "http://127.0.0.1:8000/predict"
+payload ={}
+
+
+def render_schema_form(schema: Type[BaseModel], form_title: str = "Input Form") -> dict:
+    inputs = {}
+    st.header(form_title)
+
+    for field_name, field in schema.model_fields.items():
+        field_type = field.annotation
+        # Use default only if defined, otherwise fallback
+        if field.default != PydanticUndefined:
+            default = field.default
+        else:
+            default = None
+
+        if field_type in (int, float):
+            value = st.number_input(field_name, value=default if default is not None else 0.0)
+        elif field_type is bool:
+            value = st.checkbox(field_name, value=default if default is not None else False)
+        else:
+            value = st.text_input(field_name, value=default if default is not None else "")
+
+        inputs[field_name] = value
+
+    return inputs
+
+payload = render_schema_form(schema, form_title = f"Input form for {model_name}")
+result = requests.post(endpoint, params={"dataset":model_name},json=payload)
+st.write(result.json())
